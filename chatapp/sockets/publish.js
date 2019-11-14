d3cbf38273f7d2f5608daa4a1589225608c1005c
hoge-int <dev11@ip-172-31-44-254.ap-northeast-1.compute.internal>
2019-11-14 18:52:41 +0900

    committed in 2019/11/14 18:52:41

--- chatapp/sockets/publish.js
+++ chatapp/sockets/publish.js
@@ -3,7 +3,7 @@
 module.exports = function (socket, io) {
     // 投稿メッセージを送信する
     socket.on('sendMessageEvent', function (data) {
-      if(data['message'] = "") return;
+      if(data['message'] == "") return;
       io.sockets.emit('receiveMessageEvent', data);
     });
 };
