71e0408f3de767ed08817acacc6ec781dd0a6022
hoge-int <dev11@ip-172-31-44-254.ap-northeast-1.compute.internal>
2019-11-14 19:38:49 +0900

    committed in 2019/11/14 19:38:49

--- chatapp/public/javascripts/publish.js
+++ chatapp/public/javascripts/publish.js
@@ -7,19 +7,27 @@ function publish() {
     // 入力されたメッセージを取得
     const message = $('.room-message_textarea').val();
     $('.room-message_textarea').val("");
+    const date = nowDate();
     // 投稿内容を送信
-    sendMessage(userName, message);
+    sendMessage(userName, message, date);
     return false;
 }
 
-function sendMessage(userName, message){
-  const data = {'userName': userName, 'message' : message};
+function nowDate(){
+  const month = {0: "Jan", 1: "Feb", 2: "Mar", 3:"Apr", 4: "May", 5: "Jun"
+    6: "Jul", 7: "Aug", 8: "Sep", 9: "Oct", 10: "Nov", 11: "Dec"};
+  var now new Date();
+  var date = now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds() + " "
+      + month[now.getMonth()] + ", " + now.getDate() + ", " + now.getFullYear();
+}
+function sendMessage(userName, message, date){
+  const data = {'userName': userName, 'message' : message, 'date' : date};
   socket.emit('sendMessageEvent', data);
 }
 
 // サーバから受信した投稿メッセージを画面上に表示する
 socket.on('receiveMessageEvent', function (data) {
-    $('#thread').prepend('<p>' + data['userName'] + "さん：　" + data['message'] + '</p>');
+    $('#thread').prepend('<p>' + data['userName'] + "さん：　" + data['message'] + "DATE: " + data['date'] + '</p>');
 });
 
 // ClientSide
