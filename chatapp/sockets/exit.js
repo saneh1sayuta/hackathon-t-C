ae7b986bc79ffd611c5d4e9e9c3da5d4187673c8
saneh1sayuta <yuta111909@yahoo.co.jp>
2019-11-14 19:59:21 +0900

    committed in 2019/11/14 19:59:21

--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -1,12 +1,12 @@
 'use strict';
 
-module.exports = function (socket, io) {
+module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
     socket.on('sendMessageEvent', function (data) {
       if (!data) {
           return;
       }
       console.log('クライアントの入力値：' + data);
-      io.sockets.emit("receiveMessageEvent", data);   //自と他クライアントに送信
+      //io.sockets.emit("receiveMessageEvent", data);   //自と他クライアントに送信
     });
 };
