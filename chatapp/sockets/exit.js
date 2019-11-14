a9d3d76e550938268d50c5b243293675210b94e4
saneh1sayuta <yuta111909@yahoo.co.jp>
2019-11-14 20:32:34 +0900

    committed in 2019/11/14 20:32:34

--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -1,22 +1,16 @@
 'use strict';
-
-//module.exports = function (server) {
+module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
-    //const socketIo = require('socket.io')(server, { wsEngine: 'ws' });
-    //const io = socketIo.listen(server);
-
-    //io.sockets.on('connection', function (socket) {
-        // 自クライアントに接続イベント（enteringMyselfEvent）を送信する
-
-        // 自クライアント以外に接続イベント（enterOtherEvent）を送信する
-        //socket.broadcast.emit('enterOtherEvent', '他のクライアントが接続しました。');
-
+    socket.on('', function (data) {
+    const socketIo = require('socket.io')(data, { wsEngine: 'ws' });
+    const io = socketIo.listen(data);
 
     socket.on('sendMessageEvent', function (data) {
       if (!data) {
           return;
       }
       console.log('クライアントの入力値：' + data);
-      //io.sockets.emit("receiveMessageEvent", data);   //自と他クライアントに送信
+      io.sockets.emit("receiveMessageEvent", data);   //自と他クライアントに送信
     });
-  };
+ });
+};
