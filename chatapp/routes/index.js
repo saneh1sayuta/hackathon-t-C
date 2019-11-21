aae33bb5e30befa6692c421f16388c9d9cd47e95
09Hiroshi <s1b101701133@toyo.jp>
2019-11-21 18:32:50 +0900

    committed in 2019/11/21 18:32:50

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -11,7 +11,11 @@ router.get('/', function(request, response, next) {
 // チャット画面の表示
 router.post('/room', function(request, response, next) {
     console.log('ユーザ名：' + request.body.userName);
+<<<<<<< Updated upstream
     response.render('room', { userName: 'request.body.userName' });
+=======
+    response.render('room', { userName: request.body.userName });
+>>>>>>> Stashed changes
 });
 
 module.exports = router;
