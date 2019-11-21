d4a1d9216d125d675e7e60d20b3d5a1a564c5838
ostuka1106 <yakugozyuuu@g.ecc.u-tokyo.ac.jp>
2019-11-21 18:07:49 +0900

    committed in 2019/11/21 18:07:49

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -7,7 +7,7 @@
 // 入室メッセージイベントを送信する
 
 　// 入力されたユーザ名を取得する
-　const userName = $("userName").val();
+　const userName = $("#userName").val();
 　// 入室メッセージイベントを送信する
 　socket.emit("sendMessageEvent",userName+"さんが入室しました");
 
