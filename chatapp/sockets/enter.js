8aab1562392f1fd39a66495af9a8fdd26d2dc39c
ostuka1106 <yakugozyuuu@g.ecc.u-tokyo.ac.jp>
2019-11-21 19:40:11 +0900

    committed in 2019/11/21 19:40:11

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -4,8 +4,8 @@ module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
     socket.on('sendMessageEvent', function (data) {
       socket.broadcast.emit("receiveMessageEvent",data);
-      socket.on('sendexist_MessageEvent', function (data) {
+    });
+      socket.on('send_exist_MessageEvent', function (data) {
         socket.broadcast.emit("receive_exist_MessageEvent",data);
-
-  });
+    });
 };
