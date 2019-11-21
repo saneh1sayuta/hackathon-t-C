367f6452dbfc170309bbec287f6e46cbed91e19d
ostuka1106 <yakugozyuuu@g.ecc.u-tokyo.ac.jp>
2019-11-21 18:38:52 +0900

    committed in 2019/11/21 18:38:52

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -12,7 +12,11 @@ router.get('/', function(request, response, next) {
 router.post('/room', function(request, response, next) {
     console.log('ユーザ名：' + request.body.userName);
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     response.render('room', { userName: 'request.body.userName' });
+=======
+    response.render('room', { userName: request.body.userName });
+>>>>>>> Stashed changes
 =======
     response.render('room', { userName: request.body.userName });
 >>>>>>> Stashed changes
